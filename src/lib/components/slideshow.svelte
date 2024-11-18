df20e2387d79ec7b38cc8411021270eea9981e61
João Nogueira <jrnogueira@edu.ulisboa.pt>
2024-11-18 17:45:01 +0000

    Fix images src

--- src/lib/components/slideshow.svelte
+++ src/lib/components/slideshow.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { onMount } from 'svelte';
 	export let images: string[];
 	export let alt: string;
@@ -30,4 +31,4 @@
 	});
 </script>
 
-<img src={images[index]} {alt} class="h-full w-full object-cover" />
+<img src="{base}{images[index]}" {alt} class="h-full w-full object-cover" />
